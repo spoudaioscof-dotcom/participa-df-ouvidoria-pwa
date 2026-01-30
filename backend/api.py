a13dda5cd394b4447d6d660aa36cda1067801d28
spoudaioscof-dotcom <spoudaioscof@gmail.com>
2026-01-30 13:58:29 -0300

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,15 @@
+from flask import Flask, request, jsonify
+import time
 
+app = Flask(__name__)
+
+@app.route("/manifestation", methods=["POST"])
+def register():
+    protocol = f"DF-{int(time.time())}"
+    return jsonify({
+        "protocol": protocol,
+        "status": "registrado"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
